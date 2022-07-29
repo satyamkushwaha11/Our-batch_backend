3cd0d24468beb5d1ce67f9eb40c29effc437749c
satyamkushwaha <satyamkushwaha1101@gmail.com>
2022-07-29 12:04:43 +0530

    adding post model

--- models/posts.models.js
+++ models/posts.models.js
@@ -1,8 +1,16 @@
-const {model,Schema} =require('mongoose')
+const { model, Schema } = require("mongoose");
 
-const postSchema=new Schema({
-    
-})
+const postSchema = new Schema({
+  title: {
+    type: String,
+  },
+  files:{
+    type:Array
+  },
+  mentions:{
+    type:Array
+  },
 
+});
 
-module.export =model("Posts",postSchema)
+module.export = model("Posts", postSchema);
