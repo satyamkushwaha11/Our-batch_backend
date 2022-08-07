0d9bee4ba7198d7f1deb9e2797c5cbcf0eb22b50
satyamkushwaha <satyamkushwaha1101@gmail.com>
2022-08-07 09:52:40 +0530

    ok

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require("express")
 const router = express.Router()
 const userControllers = require("../controllers/auth.controller")
 
+
+router.get('/', function(req, res, next) {  return res.status(200).json({   message:"ok" }) })
 router.post("/signup", userControllers.signup)
 router.post("/login", userControllers.login)
 // router.post("/forget-password", userControllers.userForgetPassword)
